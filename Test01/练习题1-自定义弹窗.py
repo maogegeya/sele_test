70b7e15085f08606967f651f33c2669a1ed31186
MG <maoge@ghcchina.com.cn>
2018-12-17 11:21:07 +0800

    shanchu

--- Test01/练习题1-自定义弹窗.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from selenium import webdriver
-import time
-driver = webdriver.Firefox()
-driver.get("http://baidu.csmayo88.com/kqys/?bd2pc-YY-csyyl-09200707")
-time.sleep(5)
-js = 'document.getElementById("swtbox").style.display="none";'
-driver.execute_script(js)
-time.sleep(3)
